465801c870ccf4eb6ea471446f16ec71c7b4db6d
R. Fadhil <ridhanfadhilah@gmail.com>
2025-01-05 01:25:46 +0700

    Project initialization

---
+++ android/app/src/main/kotlin/com/hightechif/flutter/wisatabandung/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hightechif.flutter.wisatabandung
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/detail_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
