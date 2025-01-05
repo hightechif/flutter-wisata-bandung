aee9cdcf9c235eaa3063501e8d26f38da60c29ff
R. Fadhil <ridhanfadhilah@gmail.com>
2025-01-05 14:57:09 +0700

    FEAT: Responsive Main Screen

--- lib/ui/screen/main/main_screen.dart
+++ lib/ui/screen/main/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisatabandung/ui/screen/main/tourism_place_grid.dart';
 import 'package:wisatabandung/ui/screen/main/tourism_place_list.dart';
 
 class MainScreen extends StatelessWidget {
@@ -7,10 +8,21 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-              "Wisata Bandung. Size: ${MediaQuery.of(context).size.width}"),
-        ),
-        body: const TourismPlaceList());
+      appBar: AppBar(
+        title:
+            const Text("Wisata Bandung"),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            return const TourismPlaceList();
+          } else if (constraints.maxWidth <= 1200) {
+            return const TourismPlaceGrid(gridCount: 4);
+          } else {
+            return const TourismPlaceGrid(gridCount: 6);
+          }
+        },
+      ),
+    );
   }
 }
